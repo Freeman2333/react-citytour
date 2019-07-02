bf59bd0f5dd20a6587924951b7045bac22e61dd6
freeman <freeman23@bigmir.net>
2019-07-02 13:50:43 +0300

    Updates

---
+++ precache-manifest.eb5ee31c84c66b55d815b2df0e8db6cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9eace579dd73b20f5b7400dc291a83d7",
+    "url": "/react-citytour/index.html"
+  },
+  {
+    "revision": "842e051ae2b300f029fa",
+    "url": "/react-citytour/static/css/main.e04f6362.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/react-citytour/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "842e051ae2b300f029fa",
+    "url": "/react-citytour/static/js/main.f05c3f88.chunk.js"
+  },
+  {
+    "revision": "8aa17204b93e9f255611",
+    "url": "/react-citytour/static/js/runtime~main.b1671487.js"
+  }
+]);
